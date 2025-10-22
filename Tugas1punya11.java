757c8c3114d79afd740536fb274045db2f6d2141
nauravlrie <nauravlrie@gmail.com>
2025-10-22 11:59:27 +0700

    first commit

---
+++ Tugas1punya11.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Tugas1punya11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String 
+    }
+    
+}
