0c13bfdad972531c84bd1b4807ee43657d83c775
nauravlrie <nauravlrie@gmail.com>
2025-10-22 15:17:38 +0700

    p1

---
+++ Tugas2punya11.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Tugas2punya11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int jenis = 0, durasi = 0, total = 0;
+
+        System.out.println("Silahkan masukkan jenis kendaraan Anda");
+        System.out.print("1. Mobil, 2. Motor, 0. Keluar");
+        jenis = sc.nextInt();
+
+        if (jenis == 1 || jenis == 2) {
+            System.out.print("Durasi parkir Anda (jam): ");
+            durasi = sc.nextInt()
+
+            
+        }
+    }
+}
